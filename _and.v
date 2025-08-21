5b70b1cb93b532e7096623bb9548f4abaf63e32d
MrDev25 <156256071+MrDeveloper25@users.noreply.github.com>
2025-08-20 23:40:34 -0600

    Add files via upload

---
+++ _and.v
@@ -0,0 +1,11 @@
+//Definicion del modulo y su entradas y salidas
+//Dentro del parentesis se definen las entradas y salidas 
+
+
+module _and(input a, input b, output c);
+//2.Definen cables o componentes internos 
+//NA
+//3. Asignaciones, instancias, conexiones
+
+assign  c= a & b;
+endmodule
